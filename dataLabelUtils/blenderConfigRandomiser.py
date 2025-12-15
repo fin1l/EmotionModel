3f004c2d3993e545e4f4718a9f42c0c57de604a2
fin1l <lumsdenfinlay@gmail.com>
2025-12-15 10:12:29 +0000

    feat: updated scene and noise generation

--- dataLabelUtils/blenderConfigRandomiser.py
+++ dataLabelUtils/blenderConfigRandomiser.py
@@ -16,12 +16,16 @@ import random
 import os
 import json
 
+# The expected format of lighting is a 3-point light set up with a key light, rim light, and fill light
 # Use HSV instead of RGB for colour (more cinematic, hues, saturation, and vibrancy represent emotion better)
 import colorsys
 
-# want high vibrancy for all of them so do not bother randomising
-def randomHSVLighting(hmin,hmax,smin,smax):
-    return colorsys.hsv_to_rgb(random.uniform(hmin, hmax), random.uniform(smin, smax), 1)
+# want high vibrancy for all of them so do not bother randomising vibrancy
+def wrapHue(hue):
+    return hue % 1.0
+
+def randomHueSat(hMin, hMax, sMin, sMax):
+    return random.uniform(hMin, hMax), random.uniform(sMin, sMax)
 # Use exp for light energy if it isn't a sun - otherwise use uniform
 # This is due to perceptual difference in intensity, use different units
 # Functions to define randomisation with an emotional bias
@@ -29,7 +33,7 @@ def happyParams(targetLightType):
     print("Happy params")
     # Bright + warm colors, normal FOV, low grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0.05, 0.15, 0.7, 1)
+    params['h'], params['s'] = randomHueSat(0.05, 0.15, 0.7, 1)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(2,4)
     else:
@@ -42,7 +46,7 @@ def sadParams(targetLightType):
     print("Sad params")
     # Cool + dark colours, normal/wide FOV, medium grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0.55, 0.65, 0.2, 0.5)
+    params['h'], params['s'] = randomHueSat(0.55, 0.65, 0.3, 0.6)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(0.1,0.5)
     else:
@@ -55,7 +59,7 @@ def angerParams(targetLightType):
     print("Angry params")
     # Red colours, low FOV, high grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0, 0.05, 0.9, 1)
+    params['h'], params['s'] = randomHueSat(0.95, 1.05, 0.9, 1)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(0.8,1.5)
     else:
@@ -68,7 +72,7 @@ def fearParams(targetLightType):
     print("Fear params")
     # Sickly + dark colours, extreme FOV, high grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0.4, 0.55, 0.3, 0.6)
+    params['h'], params['s'] = randomHueSat(0.4, 0.55, 0.4, 0.7)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(0.05,0.2)
     else:
@@ -85,7 +89,7 @@ def calmParams(targetLightType):
     print("Calm params")
     # Soft colours (low saturation), wide FOV, low grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0, 1, 0.1, 0.3)
+    params['h'], params['s'] = randomHueSat(0.5, 0.7, 0.2, 0.4)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(2,4)
     else:
@@ -99,7 +103,7 @@ def cozyParams(targetLightType):
     # deep, warm colours, low FOV, low/med grain
     # Ideally this uses a point light and not a sun for 'coziness'
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0, 0.1, 0.8, 1)
+    params['h'], params['s'] = randomHueSat(0.0, 0.1, 0.8, 1)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(0.05, 0.3)
     else:
@@ -112,7 +116,7 @@ def amazementParams(targetLightType):
     print("Amazement params")
     # deep, cool colours, high FOV, low grain
     params = {}
-    params['light_r'], params['light_g'], params['light_b'] = randomHSVLighting(0.6, 0.75, 0.8, 1)
+    params['h'], params['s'] = randomHueSat(0.6, 0.75, 0.8, 1)
     if targetLightType == 'SUN':
         params['light_energy'] = random.uniform(3, 5)
     else:
@@ -127,38 +131,159 @@ def isLight(self, object):
 
 parameterGenerationFunctions = [happyParams, sadParams, angerParams, fearParams, calmParams, cozyParams, amazementParams]
 
+import bpy
+
+def setupNoiseCompositor(scene, grain_intensity):
+    if grain_intensity <= 0:
+        return
+    scene.render.use_compositing = True
+    # Create compositor node tree if it doesn't exist
+    if scene.compositing_node_group is None:
+        tree = bpy.data.node_groups.new(name="Compositing", type='CompositorNodeTree')
+        tree.interface.new_socket(
+            name="Image",
+            description="Color output",
+            in_out='OUTPUT',
+            socket_type='NodeSocketColor'
+        )
+        scene.compositing_node_group = tree
+    else:
+        tree = scene.compositing_node_group
+    
+    # reset nodes
+    #if tree.nodes:
+    #    tree.nodes.clear()
+    
+    # Take render input
+    renderLayersNode = None
+    for node in tree.nodes:
+        if node.bl_idname == 'CompositorNodeRLayers':
+            renderLayersNode = node
+            break
+    
+    if not renderLayersNode:
+        renderLayersNode = tree.nodes.new('CompositorNodeRLayers')
+        print(f"renderLayersNode: {renderLayersNode.bl_idname}")
+        renderLayersNode.location = (-400, 200)
+    
+    # Create + setup white noise
+    whiteNoiseNode = None
+    for node in tree.nodes:
+        if node.bl_idname == 'ShaderNodeTexWhiteNoise':
+            whiteNoiseNode = node
+            break
+    
+    if not whiteNoiseNode:
+        whiteNoiseNode = tree.nodes.new('ShaderNodeTexWhiteNoise')
+        print(f"whiteNoiseNode: {whiteNoiseNode.bl_idname}")
+        whiteNoiseNode.location = (-400, -100)
+        whiteNoiseNode.noise_dimensions = '3D'
+    
+    # Create alpha over node to combine noise
+    alphaOverNode = None
+    for node in tree.nodes:
+        if node.bl_idname == 'CompositorNodeAlphaOver':
+            alphaOverNode = node
+            break
+    
+    if not alphaOverNode:
+        alphaOverNode = tree.nodes.new('CompositorNodeAlphaOver')
+        print(f"alphaOverNode: {alphaOverNode.bl_idname}")
+        alphaOverNode.location = (0, 100)
+    # Set grain intensity - 0.1 is max
+    alphaOverNode.inputs[2].default_value = 0.1 * grain_intensity
+    
+    # Add render layers and noise into the alpha over
+    if renderLayersNode.outputs['Image'] and alphaOverNode.inputs[0]:
+        tree.links.new(renderLayersNode.outputs['Image'], alphaOverNode.inputs[0])
+    if whiteNoiseNode.outputs['Color'] and alphaOverNode.inputs[1]:
+        tree.links.new(whiteNoiseNode.outputs['Color'], alphaOverNode.inputs[1])
+
+    # Map alpha over into output
+    groupOutputNode = None
+    for node in tree.nodes:
+        if node.bl_idname == 'NodeGroupOutput':
+            groupOutputNode = node
+            break
+    
+    if not groupOutputNode:
+        groupOutputNode = tree.nodes.new('NodeGroupOutput')
+        print(f"groupOutputNode: {groupOutputNode.bl_idname}")
+        groupOutputNode.location = (300, 100)
+
+    if alphaOverNode.outputs['Image'] and groupOutputNode.inputs[0]:
+        tree.links.new(alphaOverNode.outputs['Image'], groupOutputNode.inputs[0])
+    print(f"graininess: {grain_intensity}")
+
 def randomiseConfig(context):
     """
     Applies random values to the scene and returns parameters
     """
     scene = context.scene
     targetLight = scene.targetLight
+    fillLight = scene.fillLight
+    rimLight = scene.rimLight
     camera = scene.camera
-    if not (targetLight and targetLight.type == 'LIGHT' and camera and camera.type == 'CAMERA'):
+    if not (targetLight and targetLight.type == 'LIGHT' and camera and camera.type == 'CAMERA'
+            and fillLight and fillLight.type == 'LIGHT' and rimLight and rimLight.type == 'LIGHT'):
         return
     emotionChoice = math.floor(random.random() * 7)
     parameters = parameterGenerationFunctions[emotionChoice](targetLight.data.type)
 
     camera.data.lens_unit = 'FOV'
     camera.data.angle = math.radians(parameters['fov'])
-
-    if scene.render.engine == 'CYCLES':
-        scene.cycles.film_noise_intensity = parameters['grain']
-        scene.cycles.use_film_noise = parameters['grain'] > 0
-    elif scene.render.engine == 'EEVEE':
-        scene.eevee.film_noise_intensity = parameters['grain']
-        scene.eevee.use_film_noise = parameters['grain'] > 0
-
-    targetLight.data.color = (parameters['light_r'],parameters['light_g'],parameters['light_b'])
+    setupNoiseCompositor(scene, parameters['grain'])
+    #scene.eevee.use_film_grain = True
+    #scene.eevee.film_grain_amount = parameters['grain']
+    #scene.eevee.film_grain_scale = 1
+    
+    targetRGB = colorsys.hsv_to_rgb(wrapHue(parameters['h']), parameters['s'], 1.0)
+    targetLight.data.color = targetRGB
     targetLight.data.energy = parameters['light_energy']
     if targetLight.data.type != 'SUN' and targetLight.data.node_tree:
         for node in targetLight.data.node_tree.nodes:
             if node.type == 'EMISSION':
                 if "Strength" in node.inputs:
-                    node.inputs["Strength"].default_value = parameters['light_energy']
+                    node.inputs["Strength"].default_value = targetLight.data.energy
                 break
     
-    return parameters
+    # Fill light should be a complementary hue
+    fillHue = wrapHue(parameters['h'] + 0.5) 
+    fillSat = parameters['s'] * 0.5 # Fill is usually less saturated
+    fillRgb = colorsys.hsv_to_rgb(fillHue, fillSat, 1.0)
+    fillLight.data.color = fillRgb
+    # fill light should be set to have a 4:1 ratio
+    fillLight.data.energy = targetLight.data.energy * 0.25
+    if fillLight.data.type != 'SUN' and fillLight.data.node_tree:
+        for node in fillLight.data.node_tree.nodes:
+            if node.type == 'EMISSION':
+                if "Strength" in node.inputs:
+                    node.inputs["Strength"].default_value = fillLight.data.energy
+                break
+
+    # rim light should be punchy and powerful - generally a slightly different hue
+    rimHue = wrapHue(parameters['h'] + 0.1) 
+    rimRgb = colorsys.hsv_to_rgb(rimHue, parameters['s'], 1.0)
+    rimLight.data.color = rimRgb
+    rimLight.data.energy = targetLight.data.energy * 0.7
+    if rimLight.data.type != 'SUN' and rimLight.data.node_tree:
+        for node in rimLight.data.node_tree.nodes:
+            if node.type == 'EMISSION':
+                if "Strength" in node.inputs:
+                    node.inputs["Strength"].default_value = rimLight.data.energy
+                break
+    
+    # Circular hue can make it difficult for a model to predict colour
+    # 0.0 is the same as 1.0 - this can be fixed by predicting sin + cos of hue and converting back
+    # Conversion back is hue = math.atan2(hueSin, hueCos)
+    outputParams = {}
+    outputParams['hueSin'] = math.sin(2 * math.pi * parameters['h'])
+    outputParams['hueCos'] = math.cos(2 * math.pi * parameters['h'])
+    outputParams['saturation'] = parameters['s']
+    outputParams['light_energy'] = parameters['light_energy']
+    outputParams['grain'] = parameters['grain']
+    outputParams['fov'] = parameters['fov']
+    return outputParams
 
 # Only exists for testing
 class WM_OT_RandomiseConfiguration(bpy.types.Operator):
@@ -243,10 +368,20 @@ class VIEW3D_PT_RandomConfigurationPanel(bpy.types.Panel):
         box = layout.box()
         box.label(text="Scene Setup")
         box.prop(scene, "targetLight")
+        box.prop(scene, "fillLight")
+        box.prop(scene, "rimLight")
         
         if scene.targetLight is None:
             layout.label(text="Please select a Target Light", icon='ERROR')
             return
+        if scene.fillLight is None:
+            layout.label(text="Please select a Fill Light", icon='ERROR')
+            return
+        if scene.rimLight is None:
+            layout.label(text="Please select a Rim Light", icon='ERROR')
+            return
+        if len(set([scene.targetLight.data,scene.fillLight.data,scene.rimLight.data]))!=3:
+            layout.label(text="All light sources must be different", icon='ERROR')
 
         box = layout.box()
         box.label(text="Batch Generation")
@@ -259,7 +394,7 @@ class VIEW3D_PT_RandomConfigurationPanel(bpy.types.Panel):
         row.operator(WM_OT_BatchGenerate.bl_idname)
         
         box = layout.box()
-        box.label(text="3. Test")
+        box.label(text="Testing")
         box.operator(WM_OT_RandomiseConfiguration.bl_idname)
 
 classes = (
@@ -274,6 +409,14 @@ def register_properties():
         name="Target Light", type=bpy.types.Object, poll=isLight
     )
     
+    bpy.types.Scene.fillLight = bpy.props.PointerProperty(
+        name="Fill Light", type=bpy.types.Object, poll=isLight
+    )
+    
+    bpy.types.Scene.rimLight = bpy.props.PointerProperty(
+        name="Rim Light", type=bpy.types.Object, poll=isLight
+    )
+
     bpy.types.Scene.batchOutputPath = bpy.props.StringProperty(
         name="Output Folder",
         description="Folder for renders and data labelling",
@@ -288,6 +431,8 @@ def register_properties():
 
 def unregister_properties():
     del bpy.types.Scene.targetLight
+    del bpy.types.Scene.fillLight
+    del bpy.types.Scene.rimLight
     del bpy.types.Scene.batchOutputPath
     del bpy.types.Scene.batchImageCount
 

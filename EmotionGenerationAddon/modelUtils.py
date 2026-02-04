730139ce75e9cadba25886b42074ce03bf1f4b08
Fin <lumsdenfinlay@gmail.com>
2026-02-04 13:33:51 +0000

    feat: removed model utils from gitignore

---
+++ EmotionGenerationAddon/modelUtils.py
@@ -0,0 +1,74 @@
+import bpy
+import sys
+import os
+import subprocess
+import json
+import math
+
+def performInference(emotionInput):
+    """
+    Runs the inference as a background process
+    """
+    # Get worker script from add on directory
+    addOnDirectory = os.path.dirname(os.path.realpath(__file__))
+    workerScript = os.path.join(addOnDirectory, "inferenceWorker.py")
+
+    # sys.executable is Blender's Python installation
+    cmd = [sys.executable, workerScript, bpy.context.scene.modelLoadingPath]
+    
+    # Add the emotion inputs to the call
+    for val in emotionInput:
+        cmd.append(str(val))
+
+    # Run the inference as a subprocess, getting the output from the console
+    try:
+        # This pauses Blender for ~0.2 seconds while the script runs
+        result = subprocess.run(
+            cmd, 
+            capture_output=True, 
+            text=True,
+            cwd=addOnDirectory
+        )
+        workerOutput = result.stdout.strip()
+        # Output from worker
+        if result.returncode != 0:
+            print(f"Worker Crash: {result.stderr}")
+            return [0.0] * 6
+            
+        try:
+            data = json.loads(workerOutput)
+        except json.JSONDecodeError:
+            print(f"Worker returned invalid JSON: {workerOutput}")
+            return [0.0] * 6
+
+        if data.get("status") == "success":
+            return data["data"]
+        else:
+            print(f"Inference Error: {data.get('message')}")
+            return [0.0] * 6
+
+    except Exception as e:
+        print(f"Subprocess Execution Failed: {e}")
+        return [0.0] * 6
+
+def mapRawOutput(rawOutput):
+    #Using the constants from the data processing:
+    #PARAMETER_MIN_MAX = {
+    #    "hueSin": [-1, 1],
+    #    "hueCos": [-1, 1],
+    #    "saturation": [0.2, 1],
+    #    "light_energy": [math.log(50), math.log(3000)],
+    #    "grain": [0, 0.4],
+    #    "fov": [25, 100]
+    #}
+    parameters = {}
+    # Calculate hue
+    hSin, hCos = rawOutput[0] * 2 - 1, rawOutput[1] * 2 - 1
+    parameters["hue"] = math.atan2(hSin, hCos) / (2 * math.pi)
+    if parameters["hue"] < 0: parameters["hue"] += 1
+    parameters["saturation"] = rawOutput[2] * 0.8 + 0.2
+    # Using log laws, log 3000 - log 50 = log 60
+    parameters["lightEnergy"] = math.exp(rawOutput[3] * math.log(60) + math.log(50))
+    parameters["grain"] = rawOutput[4] * 0.4
+    parameters["fov"] = rawOutput[5] * 75 + 25
+    return parameters
